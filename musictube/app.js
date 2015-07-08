0e0b9e64c09ed29f4409debcbd97809388e60467
hseirafi@gmail.com <hseirafi@gmail.com>
2015-07-08 14:09:59 -0700

    commit

--- musictube/app.js
+++ musictube/app.js
@@ -5,7 +5,7 @@ function AppController($scope) {
 }
 app.run(function () {
     var tag = document.createElement('script');
-    tag.src = "http://www.youtube.com/iframe_api";
+    tag.src = "https://www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 });
